a3d97b2b93477282ca19ef1bdc86fe41e7db9223
Forrest Bowman <sexymacman@icloud.com>
2017-12-09 16:13:59 -0800

    This modification is from Zanzer on cheatengine.org modified monoscript.lua to expand class and bases faster http://forum.cheatengine.org/viewtopic.php?t=592937

--- Cheat Engine/bin/autorun/monoscript.lua
+++ Cheat Engine/bin/autorun/monoscript.lua
@@ -2482,16 +2482,44 @@ function monoform_miFindClick(sender)
   monoForm.FindDialog.execute()
 end
 
-
+-- replacing this function with a function to allow all to expand except the base functions
 function monoform_miExpandAllClick(sender)
-  if messageDialog(translate("Are you sure you wish to expand the whole tree? This can take a while and Cheat Engine may look like it has crashed (It has not)"), mtConfirmation, mbYes, mbNo)==mrYes then
+  if messageDialog("Are you sure you wish to expand the whole tree? This can take a while and Cheat Engine may look like it has crashed (It has not)", mtConfirmation, mbYes, mbNo)==mrYes then
     monoForm.TV.beginUpdate()
-    monoForm.autoExpanding=true --special feature where a base object can contain extra lua variables
-    monoForm.TV.fullExpand()
-    monoForm.autoExpanding=false
+    --monoForm.autoExpanding=true --special feature where a base object can contain extra lua variables
+    --monoForm.TV.fullExpand()
+    local tv = monoForm.TV
+    local level = 0
+    local index = 0
+    if tv.Selected ~= nil then
+      index = tv.Selected.AbsoluteIndex + 2
+      level = tv.Selected.Level
+      tv.Selected.Expand(false)
+    end
+    while index < tv.Items.Count do
+      local node = tv.Items[index]
+      if node.Level <= level then
+        break
+      end
+      if node.HasChildren and node.Level <= 3 then
+        node.Expand(false)
+      end
+      index = index + 1
+    end
+    --monoForm.autoExpanding=false
     monoForm.TV.endUpdate()
   end
 end
+
+-- function monoform_miExpandAllClick(sender)
+--  if messageDialog(translate("Are you sure you wish to expand the whole tree? This can take a while and Cheat Engine may look like it has crashed (It has not)"), mtConfirmation, mbYes, mbNo)==mrYes then
+--    monoForm.TV.beginUpdate()
+--    monoForm.autoExpanding=true --special feature where a base object can contain extra lua variables
+--    monoForm.TV.fullExpand()
+--    monoForm.autoExpanding=false
+--    monoForm.TV.endUpdate()
+--  end
+--  end
 
 function monoform_miSaveClick(sender)
   if monoForm.SaveDialog.execute() then
